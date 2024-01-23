6cc2aec686fc6df79ae2ef492f01f84f86e2db85
fluorjo <fluorjo@naver.com>
2024-01-24 02:06:42 +0900

    component와 prop

--- App.tsx
+++ App.tsx
@@ -1,9 +1,78 @@
 // 시작 전 App.js 코드
-import {StyleSheet, View} from 'react-native';
+import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+const Header = (props: {title: string}) => {
+  return <Text>{props.title}</Text>;
+};
+const MyProfile = () => {
+  return (
+    <Profile
+      name="me"
+      uri="https://pbs.twimg.com/media/FtsxswzaUAAZXJj.jpg:large"
+      profileSize={40}
+    />
+  );
+};
+const Division = () => {
+  return <Text>Division</Text>;
+};
+const FriendSection = () => {
+  return <Text>FriendSection</Text>;
+};
+const FriendList = () => {
+  return (
+    <View>
+      <Profile
+        name="ss"
+        uri="https://pbs.twimg.com/media/FtsxswzaUAAZXJj.jpg:large"
+        profileSize={30}
+      />
+      <Profile
+        name="ss"
+        uri="https://static.thenounproject.com/png/138926-200.png"
+        profileSize={30}
+      />
+      <Profile
+        name="ss"
+        uri="https://static.thenounproject.com/png/138926-200.png"
+        profileSize={30}
+      />
+
+      <Profile
+        name="ss"
+        uri="https://static.thenounproject.com/png/138926-200.png"
+        profileSize={30}
+      />
+    </View>
+  );
+};
+
+const Profile = (props: {name: string; uri: string; profileSize: number}) => {
+  return (
+    <View style={{flexDirection: 'row'}}>
+      <Image
+        source={{uri: props.uri}}
+        style={{
+          width: props.profileSize,
+          height: props.profileSize,
+        }}
+      />
+      <Text>{props.name}</Text>
+    </View>
+  );
+};
+
 
 export default function App() {
-  // eslint-disable-next-line react/react-in-jsx-scope
-  return <View style={styles.container} />;
+  return (
+    <View style={styles.container}>
+      <Header title="friend" />
+      <MyProfile />
+      <Division />
+      <FriendSection />
+      <FriendList />
+    </View>
+  );
 }
 const styles = StyleSheet.create({
   container: {
