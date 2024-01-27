47ad7a819d7fb5a5c3e85084f2c25a1a410b8c65
fluorjo <fluorjo@naver.com>
2024-01-28 06:33:38 +0900

    useEffect 초기 설정

---
+++ UseEffectWithClassComponent.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+interface State {
+  count: number;
+  what: number;
+}
+class Component extends React.Component<{}, State> {
+  constructor(props: {}) {
+    console.log('constructor');
+    super(props);
+    this.state = {
+      count: 0,
+      what: 0,
+    };
+  }
+
+  componentDidMount() {
+    console.log('didMount');
+  }
+
+  componentDidUpdate(prevProps: any, prevState: any) {
+    console.log('prevProps', prevProps);
+    console.log('prevState', prevState);
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+  render() {
+    console.log('render');
+    return (
+      <View>
+        <Text>You clicked {this.state.count} times</Text>
+        <Button
+          title="Click me"
+          onPress={() => this.setState({count: this.state.count + 1})}
+        />
+      </View>
+    );
+  }
+}
+
+export default Component;
