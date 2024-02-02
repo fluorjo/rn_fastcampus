ee1a8f50a1f2062f95ec7e0b60da14ed181dd1d6
fluorjo <fluorjo@naver.com>
2024-02-02 16:44:05 +0900

    profile

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet,View } from "react-native";
 import {
   getBottomSpace,
   getStatusBarHeight,
 } from "react-native-iphone-x-helper";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import Header from "./src/Header.js"
+import Header from "./src/Header.js";
+import MyProfile from "./src/MyProfile.js";
+import { myProfile } from "./src/data.js";
+import Margin from "./src/Margin.js";
+
 const statusBarHeight = getStatusBarHeight(true);
 const bottomSpace = getBottomSpace();
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView edges={["right", "left"]} style={styles.container}>
-        <Header />
-      </SafeAreaView>
-      {/* <View style={styles.container}>
-        <Header />
-      </View> */}
-    </SafeAreaProvider>
+    <View style={styles.container}>
+      <Header />
+      <Margin height={10}/>
+
+      <MyProfile
+        uri={myProfile.uri}
+        name={myProfile.name}
+        introduction={myProfile.introduction}
+      />
+    </View>
   );
 }
 
--- src/Profile.js
+++
@@ -1,9 +0,0 @@
-export default (props) => {
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-      }}
-    ></View>
-  );
-};
