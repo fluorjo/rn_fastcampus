2853ea1aa0aaba8d79489cfa31bcb8a2cd4d96d8
fluorjo <fluorjo@naver.com>
2024-02-03 13:25:07 +0900

    friends list, profile

--- App.js
+++ App.js
@@ -1,33 +1,41 @@
 import React from "react";
-import { StyleSheet,View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import {
   getBottomSpace,
   getStatusBarHeight,
 } from "react-native-iphone-x-helper";
-import Header from "./src/Header.js";
-import MyProfile from "./src/MyProfile.js";
-import { myProfile } from "./src/data.js";
-import Margin from "./src/Margin.js";
 import Division from "./src/Division.js";
+import FriendList from "./src/FriendList.js";
+import FriendSection from "./src/FriendSection";
+import Header from "./src/Header.js";
+import Margin from "./src/Margin.js";
+import Profile from "./src/Profile.js";
+import { friendProfiles, myProfile } from "./src/data.js";
 
 const statusBarHeight = getStatusBarHeight(true);
 const bottomSpace = getBottomSpace();
-
+const onPressArrow = () => {
+  setIsOpened(!isOpened);
+};
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
-      <Margin height={10}/>
+      <Margin height={10} />
 
-      <MyProfile
+      <Profile
         uri={myProfile.uri}
         name={myProfile.name}
         introduction={myProfile.introduction}
       />
 
-      <Margin height={15}/>
-      <Division/>
-      <Margin height={12}/>
+      <Margin height={15} />
+      <Division />
+
+      <FriendSection friendProfileLen={friendProfiles.length} onPressArrow />
+
+      <FriendList data={friendProfiles} />
+      <Margin height={12} />
     </View>
   );
 }
@@ -37,5 +45,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     paddingTop: statusBarHeight,
+    paddingHorizontal: 15,
   },
 });
