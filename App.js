03f81d1f5adbb23da4b788f96736196b804f0dd4
fluorjo <fluorjo@naver.com>
2024-02-05 07:12:05 +0900

    tabBar

--- App.js
+++ App.js
@@ -10,12 +10,13 @@ import FriendSection from "./src/FriendSection";
 import Header from "./src/Header.js";
 import Margin from "./src/Margin.js";
 import Profile from "./src/Profile.js";
+import TabBar from "./src/TabBar.js";
 import { friendProfiles, myProfile } from "./src/data.js";
-
 const statusBarHeight = getStatusBarHeight(true);
 const bottomSpace = getBottomSpace();
 export default function App() {
   const [isOpened, setIsOpened] = useState(true);
+  const [selectedTabIdx, setSelectedTabIdx] = useState(0);
 
   const onPressArrow = () => {
     setIsOpened(!isOpened);
@@ -23,26 +24,31 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Header />
-      <Margin height={10} />
+      <View style={{ flex: 1, paddingHorizontal: 15 }}>
+        <Header />
+        <Margin height={10} />
 
-      <Profile
-        uri={myProfile.uri}
-        name={myProfile.name}
-        introduction={myProfile.introduction}
+        <Profile
+          uri={myProfile.uri}
+          name={myProfile.name}
+          introduction={myProfile.introduction}
+        />
+
+        <Margin height={15} />
+        <Division />
+
+        <FriendSection
+          friendProfileLen={friendProfiles.length}
+          onPressArrow={onPressArrow}
+          isOpened={isOpened}
+        />
+
+        <FriendList data={friendProfiles} isOpened={isOpened} />
+      </View>
+      <TabBar 
+      selectedTabIdx={selectedTabIdx}
+      setSelectedTabIdx={setSelectedTabIdx}
       />
-
-      <Margin height={15} />
-      <Division />
-
-      <FriendSection
-        friendProfileLen={friendProfiles.length}
-        onPressArrow={onPressArrow}
-        isOpened={isOpened}
-      />
-
-      <FriendList data={friendProfiles} isOpened={isOpened} />
-      <Margin height={12} />
     </View>
   );
 }
@@ -52,6 +58,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     paddingTop: statusBarHeight,
-    paddingHorizontal: 15,
   },
 });
